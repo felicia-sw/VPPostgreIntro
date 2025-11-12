d86ad966484d9d546e634a040afc3be4b3fe6dbe
felicia-sw <felicia.sword@gmail.com>
2025-11-12 13:44:18 +0700

    submit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { PORT } from "./utils/env-util";
+import { publicRouter } from "./routes/public-api";
+import { errorMiddleware } from "./middlewares/error-middleware";
 
 const app = express()
 
+
 app.use(express.json());
+app.use("/api", publicRouter)
+app.use(errorMiddleware) // harus taroh paling bawah, supaya sesuai urutan
 
 app.listen(PORT || 3000, () => {
     console.log('Connected to port ${PORT}')
