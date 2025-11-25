33fddb4301bf208d80f433c37696bca99f7d6654
felicia-sw <felicia.sword@gmail.com>
2025-11-25 11:27:13 +0700

    update dari yang 18/11/2025 tapi masih bug pas di test di postman

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT } from "./utils/env-util";
 import { publicRouter } from "./routes/public-api";
 import { errorMiddleware } from "./middlewares/error-middleware";
+import { privateRouter } from "./routes/private-api";
 
 const app = express()
 
@@ -13,3 +14,8 @@ app.use(errorMiddleware) // harus taroh paling bawah, supaya sesuai urutan
 app.listen(PORT || 3000, () => {
     console.log('Connected to port ${PORT}')
 })
+
+app.use(express.json())
+app.use("/api", publicRouter)
+app.use("/api", privateRouter) //tambahin routenya disini tepat urutannya disini
+app.use(errorMiddleware)
